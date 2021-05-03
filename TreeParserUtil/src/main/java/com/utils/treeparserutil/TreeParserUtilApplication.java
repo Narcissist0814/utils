ffe652aee07a46982cc12a7aeaba4bfde59466bd
narcissist <narcissist0814@163.com>
2021-05-03 10:08:38 +0800

    first commit

---
+++ TreeParserUtil/src/main/java/com/utils/treeparserutil/TreeParserUtilApplication.java
@@ -0,0 +1,12 @@
+package com.utils.treeparserutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeParserUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeParserUtilApplication.class, args);
+    }
+}
---
+++ TreeParserUtil/src/test/java/com/utils/treeparserutil/TreeParserUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utils.treeparserutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeParserUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
